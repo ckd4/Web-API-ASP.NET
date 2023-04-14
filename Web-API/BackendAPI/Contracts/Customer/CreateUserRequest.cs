b290661c57e855fa558aebb05bb628136f2b26b0
Wayne <ckd4ff@gmail.com>
2023-04-14 10:51:49 +0300

    5

---
+++ Web-API/BackendAPI/Contracts/Customer/CreateUserRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace BackendAPI.Contracts.Customer
+{
+    public class CreateUserRequest
+    {
+        public CreateUserRequest()
+        {
+            CustomerPhone = string.Empty;
+            CustomerPassword = string.Empty;
+            CustomerFirstName = string.Empty;
+            CustomerLastName = string.Empty;
+            CustomerAddress = string.Empty;
+        }
+
+        public string CustomerPhone { get; set; }
+        public string CustomerPassword { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+}
