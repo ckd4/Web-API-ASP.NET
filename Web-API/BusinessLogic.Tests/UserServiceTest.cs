13f2e5f694f0eca0e14f2ffc2ab956bc04f50773
Wayne <ckd4ff@gmail.com>
2023-05-12 05:52:16 +0300

    Task7

---
+++ Web-API/BusinessLogic.Tests/UserServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLogic.Services;
+using Domain.Interfaces;
+using Domain.Models;
+using Domain.Wrapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Tests
+{
+    public class UserServiceTest
+    {
+        private readonly UserService service;
+        private readonly Mock<IUserRepository> userRepositoryMoq;
+
+        public UserServiceTest()
+        {
+            var repositoryWrapperMoq = new Mock<IRepositoryWrapper>();
+            userRepositoryMoq = new Mock<IUserRepository>();
+
+            repositoryWrapperMoq.Setup(x => x.Customer)
+                .Returns(userRepositoryMoq.Object);
+
+            service = new UserService(repositoryWrapperMoq.Object);
+        }
+
+
+        [Fact]
+        public async Task CreateAsync_NullUser_Exception()
+        {
+            var ex = await Assert.ThrowsAnyAsync<ArgumentNullException>(() => service.Create(null));
+
+            Assert.IsType<ArgumentNullException>(ex);
+            userRepositoryMoq.Verify(x => x.Create(It.IsAny<Customer>()), Times.Never);
+
+        }
+    }
+}
